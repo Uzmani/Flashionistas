88f01192655cfa89fbb18668dad292430fe9a100
dbc-warrior <apprentice@devbootcamp.com>
2013-08-30 15:03:22 -0700

    more routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,23 +1,35 @@
+#home page, shows login
 get '/' do
   # Look in app/views/index.erb
   erb :signin
 end
 
+#login happens
 post '/user' do
 # here,  we will authenticate user and if success, assign user_id to sessions
+  redirect '/decks'
+end
+
+#select a deck
+get '/decks' do
   erb :flashboard
 end
 
-get '/deck/:deck_id' do
+get '/decks/:deck_id' do
   #assign deck and user to new game instance
   erb :card #playing_board
 end
 
+#show first/next card of deck
+get '/game/:deck_id/:card_id' do
+  
+end
+
 #playing the game
-post '/game/deck/' do
+post '/game/:deck_id/:card_id' do
   #params[:answer], check if right or wrong
-  #wrong answer route
-  #next card route
+  #wrong answer route, shows same card
+  #right answer route, deal next card
 end
 
 #ending the game
