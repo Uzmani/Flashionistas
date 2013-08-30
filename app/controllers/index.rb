b7009270a91c4c945bf2471c260d0bb8d4bade65
dbc-warrior <apprentice@devbootcamp.com>
2013-08-30 14:40:20 -0700

    add post_user route

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,13 @@ get '/' do
   # Look in app/views/index.erb
   erb :signin
 end
+
+post '/user' do
+# here,  we will authenticate user and if success, assign user_id to sessions
+
+  erb :flashboard
+end
+
+get '/deck/:id'
+
+end
