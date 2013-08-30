3fc4614f7a7bcd744a37af24022eb1ccede11e66
dbc-warrior <apprentice@devbootcamp.com>
2013-08-30 15:07:11 -0700

    more routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -10,26 +10,29 @@ post '/user' do
   redirect '/decks'
 end
 
-#select a deck
+#go to decks
 get '/decks' do
   erb :flashboard
 end
 
+#select a deck
 get '/decks/:deck_id' do
   #assign deck and user to new game instance
   erb :card #playing_board
 end
 
 #show first/next card of deck
+#post a guess
 get '/game/:deck_id/:card_id' do
   
 end
 
-#playing the game
+#check guess
 post '/game/:deck_id/:card_id' do
   #params[:answer], check if right or wrong
   #wrong answer route, shows same card
   #right answer route, deal next card
 end
 
-#ending the game
+#game is finished
+get /game/:deck_id/finished!
