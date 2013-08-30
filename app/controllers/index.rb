1c8c94fc09601c2db7700608616d693b2c2bbfd0
dbc-warrior <apprentice@devbootcamp.com>
2013-08-30 14:52:13 -0700

    add deck and game routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,10 +5,19 @@ end
 
 post '/user' do
 # here,  we will authenticate user and if success, assign user_id to sessions
-
   erb :flashboard
 end
 
-get '/deck/:id'
-
+get '/deck/:deck_id' do
+  #assign deck and user to new game instance
+  erb :card #playing_board
 end
+
+#playing the game
+post '/game/deck/' do
+  #params[:answer], check if right or wrong
+  #wrong answer route
+  #next card route
+end
+
+#ending the game
