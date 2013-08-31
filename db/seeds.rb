5a11d07285c7f102b1297ef3e17b7087b83387fa
Apprentice <apprentice@devbootcamp.com>
2013-08-30 20:11:33 -0700

    updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,25 +4,16 @@ User.create(user_name: "Nathan", password_hash: Password.create("123"))
 deck1 = Deck.create(name: "Celebs")
 deck2 = Deck.create(name: "Cuisine")
 
-deck1 << Card.create(definition: "Blake Lively", answer: "HOT")
-deck1 << Card.create(definition: "Nicholas Cage", answer: "NOT")
-deck1 << Card.create(definition: "Amanda Bynes", answer: "NOT")
-deck1 << Card.create(definition: "Lindsay Lohan", answer: "NOT")
-deck1 << Card.create(definition: "Jack Black", answer: "NOT")
-deck1 << Card.create(definition: "Tanner Welch", answer: "HOT")
-deck1 << Card.create(definition: "Sherif Abushadi (sp?)", answer: "HOT")
-deck1 << Card.create(definition: "Anne Spalding", answer: "HOT")
-deck1 << Card.create(definition: "Barack Obama", answer: "HOT")
-deck1 << Card.create(definition: "Leaving your dishes in the sink at DBC", answer: "NOT")
-
-
-<<<<<<< HEAD
-
-
-=======
-User.create(user_name: "Nathan", password: "123")
->>>>>>> master
-
+deck1.cards << Card.create(definition: "Blake Lively", answer: "HOT")
+deck1.cards << Card.create(definition: "Nicholas Cage", answer: "NOT")
+deck1.cards << Card.create(definition: "Amanda Bynes", answer: "NOT")
+deck1.cards << Card.create(definition: "Lindsay Lohan", answer: "NOT")
+deck1.cards << Card.create(definition: "Jack Black", answer: "NOT")
+deck1.cards << Card.create(definition: "Tanner Welch", answer: "HOT")
+deck1.cards << Card.create(definition: "Sherif Abushadi (sp?)", answer: "HOT")
+deck1.cards << Card.create(definition: "Anne Spalding", answer: "HOT")
+deck1.cards << Card.create(definition: "Barack Obama", answer: "HOT")
+deck1.cards << Card.create(definition: "Leaving your dishes in the sink at DBC", answer: "NOT")
 
 
 
