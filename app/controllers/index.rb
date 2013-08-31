7a5a13ca5f3aef108c3d1b23c7a1fc594c1a4ed5
Apprentice <apprentice@devbootcamp.com>
2013-08-30 22:02:10 -0700

    added more logic for game

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -18,20 +18,16 @@ end
 #assign deck and user to new game instance
 get '/deck/:deck_id' do
   game = Game.create(user_id: session[:user_id], deck_id: params[:deck_id])
-  @cards = game.card_shuffle
-
-  redirect "/game/#{game.id}"
+  session[:game_cards] = game.get_cards
+  redirect "/game"
 end
 
 
 #begin game
 #show FIRST/NEXT card of deck
-get '/game/:game_id' do
-  @card = game.get_card
-
-
-  game = Game.find(params[:game_id])
-  redirect '/success' if game.available_cards.empty?
+get '/game' do
+  @card = session[:game_cards].last
+  redirect '/success' if session[:game_cards].empty?
   
   erb :card
 end
@@ -39,19 +35,10 @@ end
 
 #check guess
 #route answer if right/wrong
-post '/game/:deck_id' do
-  #params[:answer]
-  @card = Card.find(params[:card_id])
-
-  if params[:card_value] == @card.answer
-    if #last card
-        #game finished
-        erb :summary #success page
-    end
-  else #if wrong answer
-    #render SAME card with "wrong answer" message
-  end
-    
+post '/game' do
+  @card = session[:game_cards].last
+  session[:game_cards].pop if params[:answer] == @card.answer
+  redirect '/game'    
 end
 
 #game is finished
